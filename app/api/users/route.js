56ce50aefd37cc7bbf2d609761f3df52eb6d1639
reza Rad <radmail90@gmail.com>
2024-06-21 00:30:25 +0330

    dsfsdfsdf

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -6,14 +6,14 @@ export const runtime = 'edge';
 
 export async function POST(request) {
     try {
-        const db = await dbConnect();
+        // const db = await dbConnect();
 
-        const newUser = new User({ name: "ali amiri", coin: 0, wallet: null, refs: [], role: "player" });
-        await newUser.save();
+        // const newUser = new User({ name: "ali amiri", coin: 0, wallet: null, refs: [], role: "player" });
+        // await newUser.save();
 
-        const users = await User.find({ _id: "66701d199455fef456fd65e6" });
+        // const users = await User.find({ _id: "66701d199455fef456fd65e6" });
 
-        return NextResponse.json({ users });
+        return NextResponse.json({ users: 1 });
     } catch (error) {
         console.error('Error fetching users:', error);
         return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
