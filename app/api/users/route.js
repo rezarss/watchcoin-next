b39d86a327f31c7a8decd195dff9262270813aa1
reza Rad <radmail90@gmail.com>
2024-06-21 00:22:35 +0330

    twa window

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -1,14 +1,16 @@
 import { NextResponse } from 'next/server';
 import dbConnect from '@/app/lib/dbConnect';
-import User from '@/app/models/User'
+import User from '@/app/models/User';
+
+export const runtime = 'edge';
 
 export async function POST(request) {
     try {
         const db = await dbConnect();
-        
-        const newUser = new User({ name: "ali amiri", coin: 0, wallet: null, refs: [], role: "player" })
-        await newUser.save()
-        
+
+        const newUser = new User({ name: "ali amiri", coin: 0, wallet: null, refs: [], role: "player" });
+        await newUser.save();
+
         const users = await User.find({ _id: "66701d199455fef456fd65e6" });
 
         return NextResponse.json({ users });
