2441216eeb7c207dfbf83560adbe9b5b603f603d
reza rad <radmail90@gmail.com>
2024-07-04 07:48:23 +0000

    api user get and post

--- app/api/user/route.js
+++ app/api/user/route.js
@@ -1,28 +1,28 @@
 import { NextResponse } from "next/server";
-import dbConnect from "../lib/dbConnect";
-import User from "../models/Users";
+import dbConnect from "../../lib/dbConnect";
+import User from "../../models/Users";
 
 export async function GET(request) {
-  //const { telegramId } = await req.json();
+  const telegramId = request.nextUrl.searchParams.get("tgid");
+  await dbConnect();
+  try {
+    const user = await User.findOne({ telegramId });
+    if (!user) {
+      return NextResponse.json(
+        { result: false, error: "User not found" },
+        { status: 404 },
+      );
+    }
+    return NextResponse.json({ result: true, data: user }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { result: false, error: error.message },
+      { status: 400 },
+    );
+  }
+}
 
-  const test = request.nextUrl.searchParams.get("test");
-
+export async function POST(request) {
+  const test = await request.json();
   return NextResponse.json({ test }, { status: 200 });
-
-  //   await dbConnect();
-  //   try {
-  //     const user = await User.findOne({ telegramId });
-  //     if (!user) {
-  //       return NextResponse.json(
-  //         { result: false, error: "User not found" },
-  //         { status: 404 },
-  //       );
-  //     }
-  //     return NextResponse.json({ result: true, data: user }, { status: 200 });
-  //   } catch (error) {
-  //     return NextResponse.json(
-  //       { result: false, error: error.message },
-  //       { status: 400 },
-  //     );
-  //   }
 }
--- app/models/Users.js
+++ app/models/Users.js
@@ -16,26 +16,41 @@ const userSchema = new mongoose.Schema(
   {
     telegramId: { type: String, required: true, unique: true },
     coins: { type: Number, default: 0 },
-    clickPower: { type: Number, default: NEXT_PUBLIC_CLICKPOWER_DEFAULT },
-    energy: { type: Number, default: NEXT_PUBLIC_ENERGY },
-    maxEnergy: { type: Number, default: NEXT_PUBLIC_MAXENERGY },
+    clickPower: {
+      type: Number,
+      default: process.env.NEXT_PUBLIC_CLICKPOWER_DEFAULT,
+    },
+    energy: { type: Number, default: process.env.NEXT_PUBLIC_ENERGY },
+    maxEnergy: { type: Number, default: process.env.NEXT_PUBLIC_MAXENERGY },
     boosters: {
       multitap: {
-        level: { type: Number, default: NEXT_PUBLIC_MULTITAP_DEFAULT_LEVEL },
-        cost: { type: Number, default: NEXT_PUBLIC_MULTITAP_DEFAULT_COST },
+        level: {
+          type: Number,
+          default: process.env.NEXT_PUBLIC_MULTITAP_DEFAULT_LEVEL,
+        },
+        cost: {
+          type: Number,
+          default: process.env.NEXT_PUBLIC_MULTITAP_DEFAULT_COST,
+        },
       },
       energyLimit: {
-        level: { type: Number, default: NEXT_PUBLIC_ENERGYLIMIT_DEFAULT_LEVEL },
-        cost: { type: Number, default: NEXT_PUBLIC_ENERGYLIMIT_DEFAULT_COST },
+        level: {
+          type: Number,
+          default: process.env.NEXT_PUBLIC_ENERGYLIMIT_DEFAULT_LEVEL,
+        },
+        cost: {
+          type: Number,
+          default: process.env.NEXT_PUBLIC_ENERGYLIMIT_DEFAULT_COST,
+        },
       },
       rechargingSpeed: {
         level: {
           type: Number,
-          default: NEXT_PUBLIC_RECHARGINGSPEED_DEFAULT_LEVEL,
+          default: process.env.NEXT_PUBLIC_RECHARGINGSPEED_DEFAULT_LEVEL,
         },
         cost: {
           type: Number,
-          default: NEXT_PUBLIC_RECHARGINGSPEED_DEFAULT_COST,
+          default: process.env.NEXT_PUBLIC_RECHARGINGSPEED_DEFAULT_COST,
         },
       },
     },
