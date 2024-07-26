b860e43be0d7e7a574d42af4bdd1b68dbd169f0d
reza rad <radmail90@gmail.com>
2024-07-26 19:39:27 +0000

    deleted full api

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    workerThreads: false,
-    cpus: 1,
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
