14bde1a170f7d2c1b2330e65df505519978e04ae
reza rad <radmail90@gmail.com>
2024-07-26 19:44:35 +0000

    windows in app/page.js checked in front browser

--- app/page.js
+++ app/page.js
@@ -7,28 +7,21 @@ export default function Home() {
   const [userId, setUserId] = useState(null);
 
   useEffect(() => {
-    if (
-      typeof window !== "undefined" &&
-      window.Telegram &&
-      window.Telegram.WebApp
-    ) {
-      const webApp = window.Telegram.WebApp;
-      webApp.ready();
-      setUserId(webApp.initDataUnsafe?.user?.id);
+    if (typeof window !== "undefined") {
+      const webApp = window.Telegram?.WebApp;
+      if (webApp) {
+        webApp.ready();
+        setUserId(webApp.initDataUnsafe?.user?.id);
+      }
     }
   }, []);
+
   return (
     <main>
       <Script
         src="https://telegram.org/js/telegram-web-app.js"
         strategy="beforeInteractive"
       />
-      {/* <h1 className="text-4xl font-bold mb-8">Telegram Mini App</h1>
-      {userId ? (
-        <p className="text-xl">شناسه کاربر شما: {userId} تمام</p>
-      ) : (
-        <p className="text-xl">Loading...</p>
-      )} */}
       <CoinClickerGame />
     </main>
   );
